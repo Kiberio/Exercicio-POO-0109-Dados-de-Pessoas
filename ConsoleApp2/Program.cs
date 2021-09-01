96ddd425023cba424fde9ccc4bc0ee39544ef33e
Kiberio <dhiroshi3@gmail.com>
2021-09-01 20:06:50 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        Dados x , y;
+
+        static void Main(string[] args)
+        {
+            //Declaração de variavel da classe Dados
+            Dados x,y;
+
+            //Instanciar a variavel
+            x = new Dados();
+            y = new Dados();
+
+            //Solicitar o nome da primeira pessoa
+            Console.WriteLine("Insira o nome da primeira pessoa:");
+            x.nome = Console.ReadLine();
+            //Solicitar a idade da primeira pessoa
+            Console.WriteLine("Insira a idade da primeira pessoa:");
+            x.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Solicitar o nome da segunda pessoa
+            Console.WriteLine("Insira o nome da segunda pessoa:");
+            y.nome = Console.ReadLine();
+            //Solicitar a idade da segunda pessoa
+            Console.WriteLine("Insira a idade da segunda pessoa:");
+            y.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (x.idade > y.idade)
+            {
+                Console.WriteLine("A pessoa mais velha é: " + x.nome);
+            }
+            if (x.idade == y.idade)
+            {
+                Console.WriteLine("As duas pessoas tema mesma idade");
+            }
+            else
+            {
+                Console.WriteLine("A pessoa mais velha é: " + y.nome);
+            }
+            Console.ReadKey();
+        }
+    }
+}
